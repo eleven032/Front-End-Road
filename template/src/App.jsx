9a60c72ee216d785577528438274218bc23ccc77
Arthur Shao <Arthur_Shao@epam.com>
2022-01-27 15:16:16 +0800

    error boundary and home page

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -5,20 +5,21 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        {/*404 page*/}
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Routes>
-
-      {/* <div>Template</div> */}
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/*404 page*/}
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
+      </ErrorBoundary>
     </Router>
   );
 }
